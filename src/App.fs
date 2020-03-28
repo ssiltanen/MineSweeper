d5dbd53d0abd64bfdc43a8887c601d190e2a8b4d
Sampo Siltanen <sampo.siltanen@solita.fi>
2020-03-28 20:15:03 +0200

    Fix open adjacent empty cells

--- src/App.fs
+++ src/App.fs
@@ -4,22 +4,28 @@ open System
 open Feliz
 open Elmish
 
-type RenderState = Unknown | Empty | HasAdjacentMines of int | Flagged | Mine
+type RenderState = Unopened | Empty | HasAdjacentMines of int | Flagged | Mine
 type Point = { x : int; y : int } with
     static member Create (x, y) = { x = x; y = y }
 type Cell = 
     { point : Point; state : RenderState; isMined : bool } with
-    static member Create point = { point = point; state = Unknown; isMined = false }
+    static member Create point = { point = point; state = Unopened; isMined = false }
 
 type State = 
     { width : int
       height : int
       mines : int
-      cells : Cell list }
+      cells : Cell list
+      isGameOver : bool }
 
 type Msg =
     | ToggleFlag of Point * RenderState
-    | Open of Point * RenderState
+    | OpenCell of Point * RenderState
+    | GameOver
+
+let width = 10
+let height = 10
+let minesCount = 10
 
 let rand = Random()
 
@@ -30,28 +36,25 @@ let assignMines count cells =
     mined @ (cells |> List.skip count) |> List.sortBy (fun c -> c.point.y, c.point.x)
 
 let init() =
-    let width = 10
-    let height = 10
-    let minesCount = 20
     let axisPoints count = [ 0 .. (count - 1) ]
     let cells = 
         List.allPairs (axisPoints width) (axisPoints height) 
         |> List.map (Point.Create >> Cell.Create) 
         |> assignMines minesCount
-    { width = width; height = height; mines = minesCount; cells = cells}, Cmd.none
+    { width = width; height = height; mines = minesCount; cells = cells; isGameOver = false }, Cmd.none
 
 let updateCell point (f : Cell -> Cell) =
     List.map (fun c -> if c.point = point then f c else c)
 
-let flagCell point state =
+let flagCell state point =
     let flag = fun c -> { c with state = Flagged }
     { state with cells = state.cells |> updateCell point flag }
 
-let unflagCell point state =
-    let unflag = fun c -> { c with state = Unknown }
+let unflagCell state point =
+    let unflag = fun c -> { c with state = Unopened }
     { state with cells = state.cells |> updateCell point unflag }
 
-let getAdjacentCells point state =
+let adjacentCells state point =
     List.allPairs [-1; 0; 1] [-1; 0; 1]
     |> List.where (fun (x, y) -> x <> 0 || y <> 0)
     |> List.map (
@@ -59,37 +62,57 @@ let getAdjacentCells point state =
         >> fun p -> state.cells |> List.tryFind (fun c -> c.point = p))
     |> List.choose id
 
-let countAdjacentMines point state =
-    getAdjacentCells point state
+let countAdjacentMines state point  =
+    adjacentCells state point
     |> List.where (fun c -> c.isMined)
     |> List.length
 
-let openCell point state =
-    let openCell = fun c ->
+let openCell state point =
+    let openCell cell =
         let newState =
-            if c.isMined then Mine
+            if cell.isMined then Mine
             else 
-                let adjacentMinesCount = countAdjacentMines point state
+                let adjacentMinesCount = countAdjacentMines state point
                 if adjacentMinesCount = 0 then Empty
                 else HasAdjacentMines adjacentMinesCount 
-        { c with state = newState }
+        { cell with state = newState }
     { state with cells = state.cells |> updateCell point openCell }
+
+let openCellAndEmptyAdjacentCells state point =
+    if state.cells |> List.exists (fun cell -> cell.point = point && cell.isMined)
+    then openCell state point, Cmd.ofMsg GameOver
+    else
+        let cellIsUnopened cell = match cell.state with | Unopened -> true | _ -> false
+        let rec openAdjacentEmptyCells point state =
+            adjacentCells state point
+            |> List.where cellIsUnopened
+            |> List.fold (fun state cell ->
+                let count = countAdjacentMines state cell.point
+                if count > 0 then openCell state cell.point
+                else openCell state cell.point |> openAdjacentEmptyCells cell.point) state
+
+        let state = openCell state point
+        match state.cells |> List.find (fun cell -> cell.point = point) |> fun cell -> cell.state with
+        | Empty -> openAdjacentEmptyCells point state, Cmd.none
+        | HasAdjacentMines _ -> state, Cmd.none
+        | state -> failwithf "Error: Opened cell had invalid state %A" state
 
 let update (msg: Msg) (state: State) =
     match msg with
-    | ToggleFlag (point, Unknown) -> flagCell point state, Cmd.none
-    | ToggleFlag (point, Flagged) -> unflagCell point state, Cmd.none
+    | ToggleFlag (point, Unopened) -> flagCell state point, Cmd.none
+    | ToggleFlag (point, Flagged) -> unflagCell state point, Cmd.none
     | ToggleFlag _ -> state, Cmd.none
-    | Open (point, Unknown) -> openCell point state, Cmd.none
-    | Open _ -> state, Cmd.none
+    | OpenCell (point, Unopened) -> openCellAndEmptyAdjacentCells state point
+    | OpenCell _ -> state, Cmd.none
+    | GameOver -> { state with isGameOver = true }, Cmd.none
 
 let cellClass = function
-    | Unknown | Flagged -> "has-background-grey-lighter"
+    | Unopened | Flagged -> "has-background-grey-lighter"
     | Empty | HasAdjacentMines _ -> "has-background-white-bis"
     | Mine -> "is-danger"
 
 let cellIconClass = function
-    | Unknown | Empty | HasAdjacentMines _ -> ""
+    | Unopened | Empty | HasAdjacentMines _ -> ""
     | Flagged -> "fas fa-exclamation"
     | Mine -> "fas fa-skull-crossbones"
 
@@ -98,7 +121,7 @@ let renderCell dispatch cell =
         prop.classes [ "button"; cellClass cell.state ]
         prop.style [ style.margin 1; style.height 30; style.width 30]
         prop.onContextMenu (fun ev -> ev.preventDefault(); ToggleFlag (cell.point, cell.state) |> dispatch)
-        prop.onClick (fun _ -> Open (cell.point, cell.state) |> dispatch)
+        prop.onClick (fun _ -> OpenCell (cell.point, cell.state) |> dispatch)
         match cell.state with
         | HasAdjacentMines count ->
             prop.text (string count)
